c872aacb2a01aab8519e452ec607ec405be573c0
Barry Owens <baz789@hotmail.com>
2014-11-09 20:35:07 +0000

    Many features including popup and css
    Styles of popup, new images etc

--- Extension_Files/createPlaylistListener.js
+++ Extension_Files/createPlaylistListener.js
@@ -1,12 +1,9 @@
-
-
-  
-function click(e) {  
-
+function clickPlaylistCreate(e) {  
 if(e.target.id=="createPlaylist")
 	{
-		
-		var localURLs = new Array();		
+		alert(' im in');
+		var localURLs = new Array();
+		// The Below code searches the page for YouTube links
 		chrome.tabs.executeScript(null,  {code:""
 		+"var bodyContent = document.body.innerHTML; "
 		+"var count = 0;"
@@ -20,52 +17,28 @@ if(e.target.id=="createPlaylist")
 					+"var linkFound = searchString.substring(linkPos,linkPos+35);"
 					+"if(linkFound != previousLink)"
 					+"{"
-						+"console.log(count+'th link found at '+linkPos +' link found is:' +linkFound);"
-						+"console.log(' id only is ' +linkFound.substring(25,linkFound.length));"
-					//	+"videoIDs.push(linkFound.substring(25,linkFound.length));"
 						+"videoIDs.push(linkFound);"
-
 					+"}"					
 					+"count++;"
 					+"var newSearchString = searchString.substring(linkPos+35,searchString.length);"
 				+"doSearch(newSearchString, linkFound);"
 				+"}"			
 		+"}"
-		+"localStorage['SequentYoutubeLinks'] = JSON.stringify(videoIDs) ;"
 		+"videoIDs;"
 		}, function(result){
-		localStorage['SequentYoutubeLinks'] = JSON.stringify(result[0]) ;
-		alert(' this is the result ' +result[0]);
-		//chrome.tabs.executeScript(null,  {code:"alert('"+result+"');" });
-			
-			// Join the character array up to make it a string			
-			//var urlsString =  result[0].join(",");
-			//Split the string using commas
-			//localURLs = urlsString.split(",");		
-			// Stringify URLs to save in local Storage
-			//localStorage['YouTubeURLs'] = JSON.stringify(localURLs);
-			
+		alert(' this is the result ' +result[0]);		
 			// Create a new tab where player will use links found
-			chrome.tabs.create({'url':"http://www.barryowens.net/sequent/player.html"}, function(tab)
-
-			//chrome.tabs.create({'url':chrome.extension.getURL("playerHome.html")}, function(tab)
-	{
-		chrome.tabs.sendRequest(tab.id, {
-										'action' : 'TransferURLS',
-										'data' : result[0]
+			chrome.tabs.create({'url':"http://www.barryowens.net/sequent/player.html?LinksFound="+result[0]}, function(tab)
+			{
+				chrome.tabs.sendRequest(tab.id, {
+										'action' : 'TransferURLS',										
 										});
-		//chrome.tabs.executeScript(tab.id, {code: "console.log(\"inside bkgd\");"});
-	});
-
-		});		
-	
-	}
-	
+			});
+		});	
+	}	
 }
 
 document.addEventListener('DOMContentLoaded', function () {
-  var divs = document.querySelectorAll('div');
-  for (var i = 0; i < divs.length; i++) {
-    divs[0].addEventListener('click', click);
-  }
+  var createButton = document.getElementById('createPlaylist');
+    createButton.addEventListener('click', clickPlaylistCreate);
 });
