e731acc2a1ddb484b03e1ebbc69f3d4d79e28c9c
Barry Owens <baz789@hotmail.com>
2015-03-14 15:06:49 +0000

    Added 'embeded' search and removing unused files
    Changed description in manifest
    removed unused files
    Added search for links in the form
    "https://www.youtube.com/embed/0B0112bvG1s"

--- Extension_Files/searchForLinks.js
+++ Extension_Files/searchForLinks.js
@@ -3,8 +3,9 @@ var linkFound;
 var regResult = false;
 var videoIDs = new Array(); 
 // Regex for YouTube links and YouTube IDs
-var ytLinkPatt = new RegExp("(?:www\\.)?youtu(?:be\\.com/watch\\?v=|\\.be/)(\\w*)(&(amp;)?[\\w\\?=]*)?");
-var idPatt = new RegExp("^[a-zA-Z0-9_-]{11}$");
+var ytLinkPatt = new RegExp("(?:www\\.)?youtu(?:be\\.com/watch\\?v=|\\.be/)([\\w\-]*)(&(amp;)?[\\w\\?=]*)?");
+var ytLinkPattEmbed = new RegExp("(?:www\\.)?youtu(?:be\\.com/embed/)([\\w\-]*)(&(amp;)?[\\w\\?=]*)?");// Checks for links like https://www.youtube.com/embed/0B0112bvG1s
+var idPatt = new RegExp("^[a-zA-Z0-9._-]{11}$");
 doSearch(bodyContent,'');
 /* 
  * Function to search the html content for YouTube links
@@ -12,13 +13,14 @@ doSearch(bodyContent,'');
  */
 function doSearch(searchString, previousLink)
 { 
-		var linkPos = searchString.search('youtube.com|youtu.be'); 
+		var linkPos = searchString.search('youtube.com|youtu.be|youtube.com/embed/'); 
 		if(linkPos!=-1) 
 		{
 			// 100 characters gives chance for any parameters that may be included in the link
 			linkFound = searchString.substring(linkPos,linkPos+100);
 			if (ytLinkPatt.test(linkFound)==true)
-			{
+			{				console.log("Found embeded ID:" +ytLinkPatt.exec(linkFound)[1]);
+
 				// Extract the YouTube ID from the link
 				var videoIdFromReg = ytLinkPatt.exec(linkFound)[1];
 				// Check for the link in the array already and confirm the ID is correct form
@@ -26,7 +28,18 @@ function doSearch(searchString, previousLink)
 				{
 				    videoIDs.push(videoIdFromReg);
 				}	
-			}// end regresult check			
+			}// Now check if the embed form of YouTube link has been found
+			else if(ytLinkPattEmbed.test(linkFound)==true)
+			{
+				// Extract the YouTube ID from the link
+				var videoIdFromReg = ytLinkPattEmbed.exec(linkFound)[1];
+				// Check for the link in the array already and confirm the ID is correct form
+				if(videoIDs.indexOf(videoIdFromReg) < 0 && idPatt.test(videoIdFromReg) == true)
+				{
+				    videoIDs.push(videoIdFromReg);
+				}	
+			}
+			// end regresult check			
 			var newSearchString = searchString.substring(linkPos+10,searchString.length);
 			doSearch(newSearchString, videoIdFromReg); // Recursively call the function, starting from posistion after youtube.com
 		} // End linkPos 		
